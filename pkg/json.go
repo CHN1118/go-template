9f19fad47f12c5acf4b4d81c0a2251d233cccbb1
CHN-XCZ <108392950+CHN1118@users.noreply.github.com>
2023-08-21 16:17:27 +0800

    Initial commit

---
+++ pkg/json.go
@@ -0,0 +1,42 @@
+package pkg
+
+import (
+	"github.com/sirupsen/logrus"
+	"go-template/log"
+)
+
+const (
+	CodeOk       = 0  // 成功
+	CodeErr      = -1 // 失败
+	CodeErrToken = -2 // token相关的异常
+)
+
+// JSONResponse represents an HTTP response which contains a JSON body.
+type JSONResponse struct {
+	// HTTP status code.
+	Code int `json:"code"`
+	// JSON represents the JSON that should be serialized and sent to the client
+	JSON interface{} `json:"data"`
+}
+
+func SuccessResponse(data interface{}) JSONResponse {
+	return JSONResponse{
+		Code: 0,
+		JSON: data,
+	}
+}
+
+// MessageResponse returns a JSONResponse with a 'message' key containing the given text.
+func MessageResponse(code int, msg, msgZh string) JSONResponse {
+	log.Log.WithFields(logrus.Fields{
+		"code":   code,
+		"msg_zh": msgZh,
+	}).Warnf(msg)
+	return JSONResponse{
+		Code: code,
+		JSON: struct {
+			Message   string `json:"message"`
+			MessageZh string `json:"message_zh"`
+		}{msg, msgZh},
+	}
+}
